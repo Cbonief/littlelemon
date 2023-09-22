87047404d7d1d3880b19b02150389b64f69f2537
cbonief <carlosboni.sa@gmail.com>
2023-09-22 11:55:56 -0300

    added menu api and booking api

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from .models import Menu, Booking
+from .serializers import MenuSerializer, UserSerializer, BookingSerializer
+from rest_framework import generics, permissions, viewsets
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
+
+   # Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated] 
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookingViewSet(viewsets.ModelViewSet):
+   queryset = Booking.objects.all()
+   serializer_class = BookingSerializer
+   permission_classes = [permissions.IsAuthenticated] 
